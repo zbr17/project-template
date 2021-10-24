e24450a3944603a482949f57bb85a04f4482e7de
zbr17 <zhang-br21@mails.tsinghua.edu.cn>
2021-10-24 09:09:55 +0800

    update gedml version = 0.1.2

--- examples/demo.py
+++ examples/demo.py
@@ -1,91 +1,117 @@
-import torch.backends.cudnn as cudnn
-import logging
-import os, sys
+from logging import warning
+import os
+import sys
+import random
 from os.path import join as opj
-workspace = os.environ["WORKSPACE"]
-
-import yaml
-import argparse
-from copy import deepcopy
-logging.getLogger().setLevel(logging.INFO)
-
+import torch
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.multiprocessing as mp
+sys.path.append(
+    os.path.abspath(
+        opj(__file__, "../../src")
+    )
+)
 from gedml.launcher.misc import ParserWithConvert
 from gedml.launcher.creators import ConfigHandler
 from gedml.launcher.misc import utils
+from torchdistlog import logging
 
-# argparser
-csv_path = os.path.abspath(opj(__file__, "../config/args.csv"))
-parser = ParserWithConvert(csv_path=csv_path, name="my project")
-opt, convert_dict = parser.render()
+def main():
+    # argparser
+    csv_path = os.path.abspath(opj(__file__, "../config/args.csv"))
+    parser = ParserWithConvert(csv_path=csv_path, name="GeDML")
+    opt, convert_dict = parser.render()
 
-# args postprocess
-opt.save_path = opj(opt.save_path, opt.save_name)
-if opt.is_resume:
-    opt.delete_old = False
+    # args postprocess
+    opt.save_path = opj(opt.save_path, opt.save_name)
+    if opt.is_resume:
+        opt.delete_old = False
 
-# hyper-parameters
-start_epoch = 0
-phase = "train"
-is_test = True
-is_save = True
+    if opt.seed is not None:
+        random.seed(opt.seed)
+        torch.manual_seed(opt.seed)
+    cudnn.deterministic = opt.is_deterministic
+    cudnn.benchmark = opt.is_benchmark
 
-
-cudnn.deterministic = True
-cudnn.benchmark = True
-
-# get confighandler
-config_root = os.path.abspath(opj(__file__, "../config/"))
-if opt.link_path is None:
-    link_root = os.path.join(config_root, "links")
-    if opt.setting is None:
-        opt.link_path = opj(link_root, "link.yaml")
+    opt.ngpus_per_node = torch.cuda.device_count()
+    if opt.is_distributed:
+        assert getattr(opt, "machine_size", False)
+        opt.world_size = opt.machine_size * opt.ngpus_per_node
+        mp.spawn(main_worker, nprocs=opt.ngpus_per_node, args=(convert_dict, opt))
     else:
-        opt.link_path = os.path.join(link_root, "link_" + opt.setting + ".yaml")
-opt.assert_path = os.path.join(config_root, "assert.yaml")
-opt.param_path = os.path.join(config_root, "param")
-opt.wrapper_path = os.path.join(config_root, "wrapper")
+        main_worker(opt.device, convert_dict, opt)
 
-config_handler = ConfigHandler(
-    convert_dict=convert_dict,
-    link_path=opt.link_path,
-    assert_path=opt.assert_path,
-    params_path=opt.param_path,
-    wrapper_path=opt.wrapper_path,
-    is_confirm_first=True
-)
+def main_worker(device, convert_dict, opt):
+    # config logger
+    logging.getLogger().setLevel(logging.INFO)
+    logging.set_dist(is_dist=opt.is_distributed)
 
-# initiate params_dict
-params_dict = config_handler.get_params_dict(
-    modify_link_dict={
-        "datasets": [
-            {"train": "{}_train.yaml".format(opt.dataset)},
-            {"test": "{}_test.yaml".format(opt.dataset)}
-        ]
-    }
-)
+    opt.device = device
+    if opt.is_distributed:
+        # initiate torch.distributed
+        opt.rank = opt.ngpus_per_node * opt.machine_rank + int(device)
+        dist.init_process_group(
+            backend=opt.dist_backend,
+            init_method=opt.dist_url,
+            world_size=opt.world_size,
+            rank=opt.rank
+        )
+    logging.info("Logging: GPU: {}".format(device))
+    print("Print: GPU: {}".format(device))
+    
+    # initiate objects
+    ## hyper-parameters
+    start_epoch = 0
+    
+    if opt.is_distributed:
+        ## split in each process
+        assert (opt.batch_size % opt.world_size) == 0, \
+            "batch-size must be an integer multiple of world-size"
+        assert (opt.num_workers % opt.world_size) == 0, \
+            "num-workers must be an integer multiple of world-size"
+        opt.batch_size = int(opt.batch_size / opt.world_size) # only for trainer
+        opt.num_workers = int(opt.num_workers / opt.world_size) # only for trainer
 
-# delete redundant options
-opt_dict = deepcopy(opt.__dict__)
-convert_opt_list = list(config_handler.convert_dict.keys())
-for k in list(opt_dict.keys()):
-    if k not in convert_opt_list:
-        opt_dict.pop(k)
+    ## get config-handler
+    config_root = os.path.abspath(opj(__file__, "../config/"))
+    opt.link_path = opj(config_root, "links", "link_{}.yaml".format(opt.setting))
+    opt.assert_path = opj(config_root, "assert.yaml")
+    opt.param_path = opj(config_root, "param")
+    opt.wrapper_path = opj(config_root, "wrapper")
 
-# modify parameters
-objects_dict = config_handler.create_all(opt_dict)
+    config_handler = ConfigHandler(
+        convert_dict=convert_dict,
+        link_path=opt.link_path,
+        assert_path=opt.assert_path,
+        params_path=opt.param_path,
+        wrapper_path=opt.wrapper_path,
+        is_confirm_first=False
+    )
 
-# get manager
-manager = utils.get_default(objects_dict, "managers")
+    ## initiate params_dict
+    params_dict = config_handler.get_params_dict(
+        modify_link_dict={
+            "datasets": [
+                {"train": "{}_train.yaml".format(opt.dataset)},
+                {"test": "{}_test.yaml".format(opt.dataset)}
+            ]
+        }
+    )
 
-# get recorder
-recorder = utils.get_default(objects_dict, "recorders")
+    # modify parameters
+    objects_dict = config_handler.create_all(change_dict=opt.__dict__)
 
-# start
-manager.run(
-    phase=phase,
-    start_epoch=start_epoch,
-    is_test=is_test,
-    is_save=is_save
-)
+    # get manager and start
+    manager = utils.get_default(objects_dict, "managers")
+    manager.run(
+        phase=opt.phase,
+        start_epoch=start_epoch,
+        is_test=opt.is_test,
+        is_save=opt.is_save,
+        warm_up=opt.warm_up,
+        warm_up_list=opt.warm_up_list
+    )
 
-
+if __name__ == "__main__":
+    main()
